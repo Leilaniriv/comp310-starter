8a0092885deedba4c35432ff2d595d42cccb177c
Leilani Rivera <leilaniriv05@gmail.com>
2024-01-29 04:17:11 +0000

    added WarmUp_9

---
+++ WarmUp_8/linked_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct node {
+    int data;
+    struct node *next;
+}
+
+void push(int new_data){
+    node new_node = new node(new_data);
+    
+
+}
+
+int main (int argc, char** argv)
+{
+    
+
+    return 0;
+}
